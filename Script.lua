7e4773183581e9a41925409e8464dc64cffee4c3
Mega-Trash <96714148+xOver02@users.noreply.github.com>
2022-05-01 22:05:21 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,10 +1,10 @@
 getgenv().key = ""
 local ClientId = game:GetService("RbxAnalyticsService"):GetClientId()
 local Hwid = {
-    [1] = "YourHwid",
-    ["YourHwid"] = 1,
-    [2] = "NewHwid",
-    ["NewHwid"] = 2,
+    [1] = "ITJRVIFEO-R5TGREDERT-RTGRFD4RT-RTFD4RRX2",
+    ["ITJRVIFEO-R5TGREDERT-RTGRFD4RT-RTFD4RRX2"] = 1,
+    [2] = "RTUYHGTJFI-5TGBVRFED-5TGVCDE-TYGBVFCD-5TGV",
+    ["RTUYHGTJFI-5TGBVRFED-5TGVCDE-TYGBVFCD-5TGV"] = 2,
 }
 local Key = {
     [1] = "xUraquiam-EUJCJNJT2NRUE6TONQILU7Q6J",
@@ -24,7 +24,7 @@ function Randomkey(v)
         end
         return Genkey
 end
-setclipboard("Hwid :"..ClientId.."\nKey : NAME-"..Randomkey(2)..Randomkey(5))
+setclipboard("Hwid :"..ClientId.."\nKey : xUraquiam-"..Randomkey(2)..Randomkey(5))
 else
     print("Not Have Hwid")
 end
